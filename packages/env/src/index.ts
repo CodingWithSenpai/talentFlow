bcc9cc4286615ae5db14f8ff949b8d29c213d87d
Neeraj Dalal <admin@nrjdalal.com>
2025-11-30 23:26:37 +0530

    test: debug vercel deployment

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -6,6 +6,7 @@ import { z } from "zod"
 if (typeof window === "undefined") {
   try {
     config({ path: path.resolve(process.cwd(), "../../.env") })
+    console.log("Loaded environment variables from .env file", process.env)
   } catch (e) {
     console.error(e)
   }
