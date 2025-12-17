b3e7e3b74ee609e19d79a22d6f042a159ac73b20
Neeraj Dalal <admin@nrjdalal.com>
2025-12-17 21:35:50 +0530

    chore: revert

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -3,10 +3,9 @@ import { env } from "@packages/env/auth"
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { openAPI } from "better-auth/plugins"
+import { getCookieDomain } from "./lib/utils"
 
-// import { getCookieDomain } from "./lib/utils"
-
-// const cookieDomain = getCookieDomain(env.HONO_APP_URL)
+const cookieDomain = getCookieDomain(env.HONO_APP_URL)
 
 export const auth = betterAuth({
   baseURL: env.HONO_APP_URL,
@@ -27,14 +26,14 @@ export const auth = betterAuth({
     },
   },
   plugins: [openAPI()],
-  // ...(cookieDomain && {
-  //   advanced: {
-  //     crossSubDomainCookies: {
-  //       enabled: true,
-  //       domain: cookieDomain,
-  //     },
-  //   },
-  // }),
+  ...(cookieDomain && {
+    advanced: {
+      crossSubDomainCookies: {
+        enabled: true,
+        domain: cookieDomain,
+      },
+    },
+  }),
 })
 
 export type Session = typeof auth.$Infer.Session
