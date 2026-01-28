0ae59ab47ba9d2a287689be80d5c850034c776cc
Neeraj Dalal <admin@nrjdalal.com>
2026-01-29 04:17:35 +0530

    chore: update domains

--- packages/auth/src/lib/utils.ts
+++ packages/auth/src/lib/utils.ts
@@ -4,25 +4,20 @@
  * (api, production, staging, canary, development, dev, test, etc.)
  *
  * @example
- * getCookieDomain("https://api.zerostarter.dev")         // ".zerostarter.dev"
- * getCookieDomain("https://api.canary.zerostarter.dev")  // ".canary.zerostarter.dev"
- * getCookieDomain("https://api.dev.zerostarter.dev")     // ".dev.zerostarter.dev"
+ * getCookieDomain("https://api.zerostarter.dev")             // ".zerostarter.dev"
+ * getCookieDomain("https://api.canary.zerostarter.dev")      // ".canary.zerostarter.dev"
+ * getCookieDomain("https://api.dev.zerostarter.dev")         // ".dev.zerostarter.dev"
  * getCookieDomain("https://api.development.zerostarter.dev") // ".development.zerostarter.dev"
- * getCookieDomain("https://api.staging.zerostarter.dev") // ".staging.zerostarter.dev"
- * getCookieDomain("https://api.test.zerostarter.dev")    // ".test.zerostarter.dev"
+ * getCookieDomain("https://api.staging.zerostarter.dev")     // ".staging.zerostarter.dev"
+ * getCookieDomain("https://api.test.zerostarter.dev")        // ".test.zerostarter.dev"
  */
-export function getCookieDomain(url: string): string | undefined {
-  const allowedSubdomains = ["api", "production", "staging", "canary", "development", "dev", "test"]
 
+export function getCookieDomain(url: string): string | undefined {
   try {
     const { hostname } = new URL(url)
+    if (hostname === "localhost" || hostname === "127.0.0.1") return undefined
     const parts = hostname.split(".")
-    if (parts.length <= 2) {
-      return undefined
-    }
-    if (!allowedSubdomains.includes(parts[0])) {
-      return undefined
-    }
+    if (parts.length <= 2) return undefined
     return `.${parts.slice(1).join(".")}`
   } catch {
     return undefined
